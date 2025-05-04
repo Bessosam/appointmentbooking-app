9770d1400ba71e0f9bbdf0727c749841c95369e3
mariaMerethe <borglundmaria@gmail.com>
2025-05-04 16:36:06 +0200

    lade till header och visning i App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 import LoginPage from './pages/LoginPage'
 import HomePage from './pages/HomePage'
 import BookingPage from './pages/BookingPage'
@@ -8,16 +9,16 @@ import MyBookingsPage from './pages/MyBookingsPage'
 
 const App = () => {
   return (
-    <div>
+    <>
+      <Header />
       <Routes>
         <Route path='/' element={<LoginPage/>}/>
         <Route path='/home' element={<HomePage/>}/>
-
         <Route path='/booking' element={<BookingPage/>}/>
         <Route path='/mybookings' element={<MyBookingsPage/>}/>
       </Routes>
-    </div>
+    </>
   );
+};
 
-}
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => (
+    <header className="bg-gray-800 text-white p-4">
+      <h1 className="text-xl">Min Header!</h1>
+    </header>
+  );
+
+  export default Header;
