b8fb135e9a5befa50472a77354c5006d625c4e51
mariaMerethe <borglundmaria@gmail.com>
2025-05-06 14:12:04 +0200

    har kommenterat ut css styling som störde layout, vi ska ju ändå styla med tailwind sen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 const Header = () => (
-    <header className="bg-gray-800 text-white p-4">
+    <header className="bg-gray-800 text-white p-4 w-full">
       <h1 className="text-xl">Min Header!</h1>
     </header>
   );
